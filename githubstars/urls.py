86a0ef3c68b35e4517ada992b799be917cdecf6a
Adriano Margarin <adriano.margarin@gmail.com>
2017-10-09 21:42:00 -0300

    View home

--- githubstars/urls.py
+++ githubstars/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from githubstars.core.views import home
+
+
 urlpatterns = [
+    url(r'^$', home, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
